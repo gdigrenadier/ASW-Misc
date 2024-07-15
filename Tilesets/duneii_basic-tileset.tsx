df549f53b831ab5da5e9aa6e3e636872edce359c
gdigrenadier <smithemerson117@gmail.com>
2024-07-15 19:26:05 +0100

    Added tilesets

---
+++ Tilesets/duneii_basic-tileset.tsx
@@ -0,0 +1,219 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="duneii_basic-tileset" tilewidth="20" tileheight="20" tilecount="196" columns="14">
+ <image source="duneii_basic-tileset.png" width="280" height="280"/>
+ <tile id="13">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="unit" value="genericSlab"/>
+  </properties>
+ </tile>
+ <tile id="41">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="team" value="4"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="unit" value="genericWall"/>
+  </properties>
+ </tile>
+ <tile id="94">
+  <properties>
+   <property name="team" value="1"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="95">
+  <properties>
+   <property name="team" value="5"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="team" value="8"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="unit" value="neutralSpiceGeyser"/>
+  </properties>
+ </tile>
+ <tile id="108">
+  <properties>
+   <property name="team" value="2"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="109">
+  <properties>
+   <property name="team" value="6"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="110">
+  <properties>
+   <property name="team" value="9"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="122">
+  <properties>
+   <property name="team" value="3"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="123">
+  <properties>
+   <property name="team" value="7"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="124">
+  <properties>
+   <property name="team" value="10"/>
+   <property name="unit" value="gameDuneSelector"/>
+  </properties>
+ </tile>
+ <tile id="145">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="147">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="151">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="159">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="161">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="162">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="164">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="165">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="173">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="174">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="175">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="176">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="178">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="187">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="188">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="189">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="190">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="large-cliff" value=""/>
+  </properties>
+ </tile>
+</tileset>
